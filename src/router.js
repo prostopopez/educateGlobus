bf5d951d7ab0faa4ff4a2827b79ac978d1ad0112
prostopopez <gtaas@mail.ru>
2022-06-19 12:27:59 +0300

    filtersleft

--- src/router.js
+++ src/router.js
@@ -2,8 +2,6 @@ import React from 'react';
 import MainPage from './pages/Main';
 import ProfilePage from './pages/Profile';
 import CoursesCatalog from './pages/CoursesCatalog';
-import AuthorsCatalog from './pages/CoursesCatalog/Authors';
-import TopicsCatalog from './pages/CoursesCatalog/Topics';
 import TestCatalog from './pages/TestCatalog';
 import TestPage from './pages/TestCatalog/TestPage';
 import AdminPage from './pages/AdminPage';
@@ -26,18 +24,6 @@ const routes = [
         exact: true,
         title: ``,
         component: CoursesCatalog
-    },
-    {
-        path: `/courses/authors`,
-        exact: true,
-        title: ``,
-        component: AuthorsCatalog
-    },
-    {
-        path: `/courses/topics`,
-        exact: true,
-        title: ``,
-        component: TopicsCatalog
     },
     {
         path: `/tests`,
